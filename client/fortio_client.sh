cfb3b3ccb8eb41e7ee1838e507d7d0e33f1f1f48
wadeling <wadeling@gmail.com>
2019-10-28 11:38:42 +0800

    add client test content

--- client/fortio_client.sh
+++ client/fortio_client.sh
@@ -8,7 +8,7 @@
 #fortio load -c 40 -t 240s -qps 40000 http://10.144.72.92:9211
 
 # all pass
-fortio load -c 30 -t 240s -qps 30000 http://10.144.72.92:9211
+#fortio load -c 30 -t 240s -qps 30000 http://10.144.72.92:9211
 
 #
 #fortio load -c 1 -t 240s -qps 1000 http://10.144.72.92:9211
@@ -17,3 +17,10 @@ fortio load -c 30 -t 240s -qps 30000 http://10.144.72.92:9211
 #### test fortio http server ####
 #fortio load -c 30 -t 240s -qps 30000 http://10.144.72.92:9300
 
+### istio test withoud payload ###
+#fortio load -c 16 -t 240s -qps 16000 http://10.144.72.92:9211
+#add envoy proxy
+fortio load -c 16 -t 240s -qps 16000 http://10.144.72.92:9210
+
+## payload test, read eof error with payload
+#fortio load -c 1 -n 10 -maxpayloadsizekb 1500 -payload-size 1000 http://10.144.72.92:9211
