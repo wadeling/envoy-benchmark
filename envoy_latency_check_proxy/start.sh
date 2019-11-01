3f4d7eb68b141f41c515f2237f62c77f76ce6d58
wadeling <wadeling@gmail.com>
2019-11-01 16:58:10 +0800

    add latency proxy

---
+++ envoy_latency_check_proxy/start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#cp newest envoy
+
+cp -af ~/go/src/github.com/wadeling/envoy/bazel-bin/source/exe/envoy-static ./
+
+#start
+
+# mydev envoy
+#./envoy-static  -c config.yaml --concurrency 2 -l critical --base-id 1
+./envoy-static  -c config.yaml --concurrency 2 -l trace --base-id 1
+#./envoy-static -c config.yaml -l trace
+
+# perf
+#perf stat -e cache-misses "/usr/bin/envoy -c config.yaml --concurrency 6 -l critical --base-id 1"
